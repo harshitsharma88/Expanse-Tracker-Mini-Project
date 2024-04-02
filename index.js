24ab08680f6223a4eac5e2658f0c19632e113669
harshitsharma88 <111192121+harshitsharma88@users.noreply.github.com>
2024-04-02 19:22:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,84 @@
-const sbbtn=document.querySelector('#btn');
-const ul=document.querySelector('ul');
-sbbtn.addEventListener('click',add);
+const arr= Object.keys(localStorage);
+
+window.onload=getAll(arr);
+
+arr.sort();
 let id=0;
+if(arr.length>0){
+
+id=parseInt(arr[arr.length-1])+1;
+}
+
+const sbbtn=document.querySelector('#btn');
+sbbtn.addEventListener('click',add);
+
+
+function getAll(arr){
+    arr.forEach(element => {
+        show(element);
+    });
+
+}
+
+
 function add(event){
     event.preventDefault();
     const valam=document.querySelector("#amount").value+"-"+
     document.querySelector('#desc').value+"-"+
     document.querySelector('#category').value;
-    
-    
-    const li=document.createElement('li');
-    li.innerHTML=`${valam } <button class='delbtn'>Delete</button><button class='edit'>Edit</button>`;
-    li.setAttribute('id',id);
     localStorage.setItem(id,valam);
+
+    show(id);
     id++;
-   
-    ul.appendChild(li);
+
+    document.querySelector("#amount").value="";
+    document.querySelector('#desc').value="";
+    document.querySelector('#category').value="";
 
 }
-ul.addEventListener('click',function(event){
-    if(event.target.className==='delbtn'){
+
+function show(id){
+    let ul=document.querySelector('ul');
+
+    const li=document.createElement('li');
+    const value= localStorage.getItem(id)+" ";
+    li.innerText=value;
+    li.setAttribute('id',id);
+
+    const dltbtn= document.createElement('button');
+    dltbtn.innerText='Delete';
+
+    const editbtn=document.createElement('button');
+    editbtn.innerText='Edit';
+
+    li.appendChild(dltbtn);
+    li.appendChild(editbtn);
+
+    ul.appendChild(li);
+
+    dltbtn.addEventListener('click',(event)=>{
+        const toRemove=event.target.parentNode.getAttribute('id');
+        localStorage.removeItem(toRemove);
+        
         ul.removeChild(event.target.parentNode);
-        const delid=event.target.parentNode.getAttribute('id');
-        localStorage.removeItem(delid);
-    }
-    else if(event.target.className==='edit'){
-        const valam=document.querySelector("#amount").value+"-"+
-    document.querySelector('#desc').value+"-"+
-    document.querySelector('#category').value;
-    const delid=event.target.parentNode.getAttribute('id');
-    localStorage.setItem(delid,valam);
-    event.target.parentNode.innerHTML=`${valam } <button class='delbtn'>Delete</button><button class='edit'>Edit</button>`;
-    }
-    
-})
+    })
+
+    editbtn.addEventListener('click',(event)=>{
+        event.preventDefault();
+        const toRemove=event.target.parentNode.getAttribute('id');
+        console.log(toRemove);
+        
+        let val=localStorage.getItem(toRemove);
+        let arritem=val.split("-");
+        document.querySelector("#amount").value=parseInt(arritem[0]);
+        document.querySelector("#desc").value=arritem[1];
+        document.querySelector("#category").value=arritem[2];
+        localStorage.removeItem(toRemove);
+
+        ul.removeChild(event.target.parentNode);
+    })
+
+
+
+}
+
